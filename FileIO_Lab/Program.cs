bbc7bc102791b3bbbbea48addea4ab94ffece3dc
Hemory Phifer <hphifer@uwm.com>
2019-10-28 13:07:13 -0400

    first commit

---
+++ FileIO_Lab/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+
+namespace FileIO_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textPath = @"C:\Users\hphifer\source\repos\FileIO_Lab\FileIO_Lab\StudentData.txt";
+
+            //Create a list of students
+            List<Student> students = new List<Student>();
+
+
+
+            //Add a student to the list
+            students.Add(new Student("Alfred", "Hitchcock", "Science"));
+
+
+            //Create a list of strings for studentCollection
+            List<string> studentCollection = new List<string>();
+
+            foreach (Student student in students)
+            {
+                studentCollection.Add($"{student.FirstName}|{student.LastName}|{student.ClassSubject}");
+            }
+
+            using (StreamWriter sw = new StreamWriter(textPath))
+            {
+                foreach (string student in studentCollection)
+                {
+                    sw.WriteLine(student);
+                }
+            }
+
+            ReadAndFormatText(textPath);
+
+            Console.ReadLine();
+
+        }
+
+        private static void ReadAndFormatText(string textPath)
+        {
+            using (StreamReader sr = new StreamReader(textPath))
+            {
+                string line = sr.ReadToEnd();
+
+                string[] textArray = line.Split('|');
+
+                foreach (string formattedLine in textArray)
+                {
+                    Console.Write($" {formattedLine}");
+                }
+            }
+        }
+    }
+}
---
+++ FileIO_Lab/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileIO_Lab
+{
+   public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassSubject { get; set; }
+
+        public Student(string firstName, string lastName, string classSubject)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ClassSubject = classSubject;
+        }
+
+    }
+}
